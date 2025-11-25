b9ec432676da1807a51ad3e1223f4480b6cb21a5
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-25 17:25:45 +0545

    Selecting flields to query

--- playground/views.py
+++ playground/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
-from store.models import Product
+from store.models import Product,OrderItem
 
 def htmlRender(request):
-    #Limiting results
-    #1.we get product upto 5 from 0 to 4
-    queryset=Product.objects.all()[:5]
+   #Selecting flields to query
+    #1.we get id and title in dictionary from
+    queryset=Product.objects.values('id','title')
 
-    #2.we get product upto 5 from 5to 10
-    queryset=Product.objects.all()[5:10]
+    #1.we get id and title and title of collection  in dictionary from
+    queryset=Product.objects.values('id','title','collection__title')
 
+    #1.we get id and title and title of collection  in tuple from
+    queryset=Product.objects.values_list('id','title','collection__title')
+
+    #.Select products that have been order and sort them by title
+    queryset=Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
 
 
