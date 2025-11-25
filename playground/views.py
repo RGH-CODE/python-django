d0d031e5758c871edfacce53008b1c904c2d6672
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-25 17:43:38 +0545

    selecting related objects:prefetch_related(for many instance relationship) and select_related(for one instance)ie combine selection of objects

--- playground/views.py
+++ playground/views.py
@@ -5,9 +5,9 @@ from django.db.models import Q,F
 from store.models import Product,OrderItem
 
 def htmlRender(request):
-   #selecting related objects:prefetch_related(for many instance relationship)
-   #eg product has many promotions
-    queryset=Product.objects.prefetch_related('promotions').all()
+   #selecting related objects:prefetch_related(for many instance relationship) and select_related(for one instance)ie combine selection of objects
+   
+    queryset=Product.objects.prefetch_related('promotions').select_related('collection').all()
    
 
     return render(request,'hello.html',{'name':'Rajesh','products':list(queryset)})
