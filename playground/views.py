e32d869cf07371c9e0e9ab2ebfa30c77ba5545b1
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-25 14:31:57 +0545

    Limiting results

--- playground/views.py
+++ playground/views.py
@@ -5,36 +5,15 @@ from django.db.models import Q,F
 from store.models import Product
 
 def htmlRender(request):
-#     #sorting data
-#     #1.sorts in alphabet ascending in title
-#     queryset=Product.objects.order_by("title")
-    
-#     #2.sorts in alphabet descending in title
-#     queryset=Product.objects.order_by("-title")
-    
-#     #3.unit price in ascending and title in descending
-#     queryset=Product.objects.order_by("unit_price","-title")
-   
-#    #3.unit price in descending and title in ascending ...due to use of reverse()
-    
-#     queryset=Product.objects.order_by("unit_price","-title").reverse()
-#     #return render(request,'hello.html',{'name':'Rajesh','products':list(queryset)})
-    
+    #Limiting results
+    #1.we get product upto 5 from 0 to 4
+    queryset=Product.objects.all()[:5]
 
-#     #4.get individual object
-#     product=Product.objects.order_by("unit_price")[0]
-    
-    
-#     #use of earliest:sorts in ascending and returns first object
-    
-    product=Product.objects.earliest("unit_price")
-#use of latest:sorts in descending and returns first object
-    
-    product=Product.objects.latest("unit_price")
-    
-    
-   
-    
-   
-    return render(request,'hello.html',{'name':'Rajesh','product':product})
+    #2.we get product upto 5 from 5to 10
+    queryset=Product.objects.all()[5:10]
+
+
+
+
+    return render(request,'hello.html',{'name':'Rajesh','products':list(queryset)})
     
