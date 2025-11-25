3fe83de6876f7427cbc83858033c714bb37af0dc
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-25 17:39:05 +0545

    selecting related objects:prefetch_related(for many instance relationship)

--- playground/views.py
+++ playground/views.py
@@ -5,12 +5,9 @@ from django.db.models import Q,F
 from store.models import Product,OrderItem
 
 def htmlRender(request):
-   #selecting related objects:select_related(for one instance relationship)
-    #1.it selects the query from product with collection.this select_related prevents from generating extra query
-    #Note select_related is used for one instance relation ie here product has one collection.
-    queryset=Product.objects.select_related('collection').all()
-   #2.to expand relation we use:
-    #queryset=Product.objects.select_related('collection__SomeOtherField').all()
+   #selecting related objects:prefetch_related(for many instance relationship)
+   #eg product has many promotions
+    queryset=Product.objects.prefetch_related('promotions').all()
    
 
     return render(request,'hello.html',{'name':'Rajesh','products':list(queryset)})
