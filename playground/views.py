65533e3ed31c7f28551fdb350e5450b11bdf9952
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-26 16:01:01 +0545

    Annotating objects and calling database functions using Func

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q,F
-from django.db.models.aggregates import Count,Max,Min,Avg,Sum
-from store.models import Product,OrderItem,Order
+from django.db.models import Q,F,Value,Func
+from django.db.models.functions import Concat 
+from store.models import Product,OrderItem,Order,Customer
 
 def htmlRender(request):
-    #aggregating objects
-    result=Product.objects.aggregate(Count('id'))
-    #proper way of showing
-    result=Product.objects.aggregate(count=Count('id'))
+   #Annotating Objects:to add additional attributes to objects while quering them 
+    # queryset=Customer.objects.annotate(is_new=True) #we get error cause we cannot pass booelan we need to pass expression like 1.value 2.F 3.Aggregate
+    #to solve this 
+    #1.Value =for simple value
+    queryset=Customer.objects.annotate(is_new=Value(True))
     
-    #to aggregate multiple fields 
-    result=Product.objects.aggregate(count=Count('id'),Minimum_price=Min('unit_price'))
+    #to create new attribute same as id of customer can be done by F object referencing related flieds 
+    queryset=Customer.objects.annotate(new_id=F('id'))
     
-    #below code selects the product whose collection id is 1 and computes two aggregates value i.e min price and id 
-    result=Product.objects.filter(collection__id=1).aggregate(count=Count('id'),Minimum_price=Min('unit_price'))
+    #to create new attribute same as id of customer increasing each by 1 can be done by F object referencing related flieds 
+    queryset=Customer.objects.annotate(new_id=F('id')+1)
     
+    #Calling Database Functions using Func
+    queryset=Customer.objects.annotate(full_name=Func(F('first_name'),Value(' '), F('last_name'),function='CONCAT'))
+    
+    #shortcut way of concatinating using cancat function
+    queryset=Customer.objects.annotate(full_name=Concat('first_name',Value(' '), 'last_name'))
 
-
-    return render(request,'hello.html',{'name':'Rajesh','result':result})
+    return render(request,'hello.html',{'name':'Rajesh','orders':list(queryset)})
     
