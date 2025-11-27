81964801aa885371b49b6f936e929e7cda1715a2
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-27 13:35:20 +0545

    custom manager

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,7 @@ from store.models import Product
 from tag.models import TagItem
 
 def htmlRender(request):
-    content_type=ContentType.objects.get_for_model(Product)
-    queryset=TagItem.objects.select_related('tag').filter(content_type=content_type,object_id=1)
+    #querying generic relationships 
+    queryset=TagItem.objects.get_tags_for(Product,1)
     return render(request,'hello.html',{'name':'Rajesh','orders':list(queryset)})
     
