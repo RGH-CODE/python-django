6304ca6bd2ab94f045a2cac9c0c0e10e4632c162
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-27 17:15:21 +0545

    updating objects

--- playground/views.py
+++ playground/views.py
@@ -6,11 +6,21 @@ from tag.models import TagItem
 
 def htmlRender(request):
     #creating objects
-    collection=Collection()
-    collection.title="Digital gadgets"
-    collection.featured_product=Product(pk=1)
+    #1.method 1 of updating
+    '''
+    collection=Collection(pk=11)
+    collection.title="Games"
+    collection.featured_product=None
     collection.save()
-    collection.id
-    
+    '''
+   
+   #probelm of using method 1 for updating here we forget to indicaate title so django sets title to empty which leads to dataloss
+    '''collection=Collection(pk=11)
+   
+    collection.featured_product=None
+    collection.save()
+    '''
+    #2. method 2 of updating update. which solves problem of data loss in database 
+    Collection.objects.filter(pk=11).update(featured_product=None)
     return render(request,'hello.html',{'name':'Rajesh'})
     
