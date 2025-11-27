9f5a709f6a599642226b8279fe9b0dc82aff5b95
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-27 17:22:07 +0545

    deleting objects

--- playground/views.py
+++ playground/views.py
@@ -5,22 +5,15 @@ from store.models import Product,Collection
 from tag.models import TagItem
 
 def htmlRender(request):
-    #creating objects
-    #1.method 1 of updating
+    #deleting objects
+    #1.to delete single known object
     '''
     collection=Collection(pk=11)
-    collection.title="Games"
-    collection.featured_product=None
-    collection.save()
+    collection.delete()
     '''
-   
-   #probelm of using method 1 for updating here we forget to indicaate title so django sets title to empty which leads to dataloss
-    '''collection=Collection(pk=11)
-   
-    collection.featured_product=None
-    collection.save()
-    '''
-    #2. method 2 of updating update. which solves problem of data loss in database 
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    #2.To delete multiple objects
+    Collection.objects.filter(id__gt=8).delete()
+    
+    
     return render(request,'hello.html',{'name':'Rajesh'})
     
