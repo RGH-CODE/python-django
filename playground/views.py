ce1c08d91ea3b62e6a96a550f6d8179ff3dde0c1
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-28 14:56:51 +0545

    Transaction: to to execute two seperate logic/code and if one fails leads to both fail .and prevents us from unnecessary orders/error/     #;;........let here is code that does not related to ordering product

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product,Collection
+from store.models import Product,Collection,Order,OrderItem
+from django.db import transaction
 from tag.models import TagItem
 
 def htmlRender(request):
-    #deleting objects
-    #1.to delete single known object
-    '''
-    collection=Collection(pk=11)
-    collection.delete()
-    '''
-    #2.To delete multiple objects
-    Collection.objects.filter(id__gt=8).delete()
-    
-    
+    #Transaction: to to execute two seperate logic/code and if one fails leads to both fail .and prevents us from unnecessary orders/error/
+    #;;........let here is code that does not related to ordering product
+    with transaction.atomic():
+        order=Order()
+        order.customer_id=1
+        order.save()
+        
+        item=OrderItem()
+        item.order=order
+        item.product_id=1
+        item.quantity=1
+        item.unit_price=10
+        item.save()
     return render(request,'hello.html',{'name':'Rajesh'})
     
