42c5a4763204ca7ffa1e7b18d3873dbcc290caf7
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-11-30 16:37:47 +0545

    model serializer i.e.shortcut of creating serializer using Meta class

--- store/serializers.py
+++ store/serializers.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers
 from decimal import Decimal
 from . models import Product,Customer,Collection
-class CollectionSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
-class ProductSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
-    price=serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id','title','featured_product']
+    
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','price','price_with_tax','collection']
+    #since id and title are same name in product model they can be removed from here 
+    price=serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price') #price name is not in product model so need to define here  
    
     #creating custom serializers fields
-    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')
+    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax') #it is not in product model
     
-    #serializing using hyperlink:
-    collection=serializers.HyperlinkedRelatedField(queryset=Collection.objects.all(),view_name="collection-detail")
     def calculate_tax(self,product:Product):
         return product.unit_price * Decimal(1.1)
+    #serializing using hyperlink:
+    collection=serializers.HyperlinkedRelatedField(queryset=Collection.objects.all(),view_name="collection-detail") #it is kept here cause we want collection in hyperlink form.But we can remove it if we want collection in primary key from
     
