add25053479f82d777ca439334163f9db7b0a01f
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-12-02 14:52:37 +0545

    advance api Class based view

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from . models import Product,Customer,Collection
 from . serializers import  ProductSerializer,CollectionSerializer
 # Create your views here.
@@ -12,56 +13,39 @@ from . serializers import  ProductSerializer,CollectionSerializer
 #This is django response
     #return HttpResponse('ok')
 #better to use rest framework reponse which is better 
-@api_view(['GET','POST'])
-def product_list(request):
-    if request.method=='GET':
+
+#creating  class for class based view 
+class ProductList(APIView):
+  def get(self,request):
       queryset=Product.objects.select_related('collection').all()
       serializer=ProductSerializer(queryset,many=True,context={'request':request})
       return Response(serializer.data)
-    elif request.method=='POST':
-        serializer=ProductSerializer(data=request.data) 
-        #method 1 of checking validation of data
-        # if serializer.is_valid():
-        #     serializer.validated_data
-        #     return Response('ok the data is deserialized!!')
-        # else:
-        #     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) #now if invalid data is posted then we get proper error
-        
-         #method 2 of checking validation of data
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        # return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.data)
+  def post(self,request):
+      serializer=ProductSerializer(data=request.data) 
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data)
 
 
-# @api_view()
-# def product_detail(request,id):
-    # try:
-    #   product=Product.objects.get(pk=id)
-    #   serializer= ProductSerializer(product)
-    #   return Response(serializer.data)
-    # except Product.DoesNotExist:
-    #     # return Response(status=404)
-    #     #best way to do 
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-#but using try and catch for all time might be long so use django shortcuts
-@api_view(["GET",'PUT','DELETE'])
-def product_detail(request,id):
-    product=get_object_or_404(Product,pk=id)
-    if request.method=="GET":
+class ProductDetail(APIView):
+  def get(self,request,id):
+      product=get_object_or_404(Product,pk=id)
       serializer=ProductSerializer(product,context={'request': request})
       return Response(serializer.data)
-    elif request.method=="PUT":
+  def put(self,request,id):
+      product=get_object_or_404(Product,pk=id)
       serializer=ProductSerializer(product,context={'request': request},data=request.data)
       serializer.is_valid(raise_exception=True)
       serializer.save()
       return Response(serializer.data)
-    elif request.method=="DELETE":
+  def delete(self,request,id):
+      product=get_object_or_404(Product,pk=id)
       if product.orderitems.count()>0:
         return Response({'error':'This product can not be deleted cause it is related with order item'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
       product.delete()
       return Response({'Success':'Product deleted successfully'},status=status.HTTP_204_NO_CONTENT)
         
+
 
 @api_view(["GET","POST"])
 def collection_list(request):
