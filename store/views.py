c18c72d663de09a77b8428141f583e7e6d3a7d1c
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-12-05 18:28:10 +0545

    Generic Filtering Product by collection id

--- store/views.py
+++ store/views.py
@@ -8,21 +8,18 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from . models import Product,Customer,Collection,OrderItem,Review
 from . serializers import  ProductSerializer,CollectionSerializer,ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
-  
+  queryset=Product.objects.all()
   serializer_class=ProductSerializer
+  filter_backends=[DjangoFilterBackend]
+  filterset_fields=['collection_id','unit_price']
   
-  def get_queryset(self):
-    queryset=Product.objects.all()
-    collection_id=self.request.query_params.get('collection_id')
-    if collection_id is not None:
-      queryset=queryset.filter(collection_id=collection_id)
-      
-    return queryset
+  
   
   def get_serializer_context(self):
       return {'request':self.request}
--- storefront/settings.py
+++ storefront/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    
+    'django_filters',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
