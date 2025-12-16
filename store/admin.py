ce693cbaf8f835724fe3df3e0584bdd3c758f4f2
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-12-16 16:49:50 +0545

    Customizing Forms on adding products by autocomplete ,prepopulated etc

--- store/admin.py
+++ store/admin.py
@@ -27,6 +27,11 @@ class InventoryFilter(admin.SimpleListFilter):
 #for product 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    exclude=['promotions']
+    prepopulated_fields={
+      'slug':['title']
+    }
+    autocomplete_fields=['collection']
     actions=['clear_inventory']
     list_display=['title','unit_price','inventory_status','collection_title']
     list_editable=['unit_price']
@@ -61,7 +66,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
   list_display=['title','products_count']
-  
+  search_fields=['title']
   @admin.display(ordering='products_count')
   def products_count(self,collection):
     url=(reverse('admin:store_product_changelist')
