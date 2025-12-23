fff39d883d5270141bfc9a44c8900db2d969ea78
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-12-23 19:57:44 +0545

    returning created order

--- store/serializers.py
+++ store/serializers.py
@@ -149,3 +149,4 @@ class CreateOrderSerializer(serializers.Serializer):
           OrderItem.objects.bulk_create(order_items)
           
           Cart.objects.filter(pk=self.validated_data['cart_id']).delete()
+          return order
