9153025e948f55cf69d04d35a317f36c868c71b1
RGH-CODE <rajesh.ghimire200@gmail.com>
2025-12-24 16:30:54 +0545

    updating get_or_create with only get cause now signal can handle create if customer does not exists and prevents error

--- store/views.py
+++ store/views.py
@@ -132,7 +132,7 @@ class CustomerViewSet(ModelViewSet):
 
     @action(detail=False,methods=["get","put"],permission_classes=[IsAuthenticated])
     def me(self,request):
-      (customer,created)=Customer.objects.get_or_create(user_id=request.user.id)
+      customer=Customer.objects.get(user_id=request.user.id)
       if request.method=="GET":
         serializer=CustomerSerializer(customer)
         return Response(serializer.data)
@@ -173,5 +173,5 @@ class OrderViewSet(ModelViewSet):
   def get_queryset(self):
     if self.request.user.is_staff:
       return Order.objects.all()
-    (customer_id,created4)=Customer.objects.get_or_create(user_id=self.request.user.id)
+    customer_id=Customer.objects.get(user_id=self.request.user.id)
     Order.objects.filter(customer_id=customer_id)
