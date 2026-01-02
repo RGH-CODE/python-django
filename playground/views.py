72b68e1d3db87bb302faf3522e6f10ee2491473b
RGH-CODE <rajesh.ghimire200@gmail.com>
2026-01-02 22:06:15 +0545

    using the low level cache api

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render 
 import requests
+from django.core.cache import cache 
 def htmlRender(request):
-    requests.get('https://httpbin.org/delay/2')
-    return render(request,'hello.html',{'name':"Rajesh"})
+    key='httpbin_result'
+    if cache.get(key) is None:
+       response=requests.get('https://httpbin.org/delay/2')
+       data=response.json()
+       cache.set(key,data)
+    return render(request,'hello.html',{'name':cache.get(key)})
     
