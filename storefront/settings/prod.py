94b29c49589aa5aa76943c329bc5397454aeb1f6
RGH-CODE <rajesh.ghimire200@gmail.com>
2026-02-02 18:41:36 +0545

    fix cloudinary duplicate

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
-INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+INSTALLED_APPS += ['cloudinary_storage']
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
