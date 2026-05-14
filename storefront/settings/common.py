8d00cfe7718286a95bbc7ca0174aecd4072e71d3
RGH-CODE <rajesh.ghimire200@gmail.com>
2026-05-14 09:53:16 +0545

    fixing errors

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -216,6 +216,8 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
+
 
 
 #for admin site mail
